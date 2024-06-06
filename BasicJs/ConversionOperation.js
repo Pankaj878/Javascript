42464a4542f33625469529969fb24a00e775fece
Pankaj Kaurav <84280158+Pankaj878@users.noreply.github.com>
2024-06-06 20:08:54 +0000

    Data type and conversion

---
+++ BasicJs/ConversionOperation.js
@@ -0,0 +1,52 @@
+let value = 10
+let valueString = String(value)
+// console.log(typeof(value))
+// console.log(typeof(valueString))
+
+let valuestring1= '101'
+let afterno = Number(valuestring1)
+//console.log(typeof(afterno));
+
+let valueString2 = '101abc'
+let afterno2 = Number(valueString2)
+// console.log(typeof(afterno2))
+// console.log(afterno2)
+
+let VS = '101afc'
+let afterBoolean = Boolean(VS)
+// console.log(typeof(afterBoolean))
+// console.log(afterBoolean)
+
+let VS2 = ''
+let afterBoolean2 = Boolean(VS2)
+// console.log(typeof(afterBoolean2))
+// console.log(afterBoolean2)
+
+VS = -1
+afterBoolean = Boolean(VS)
+// console.log(typeof(afterBoolean))
+// console.log(afterBoolean)
+// console.log(VS)
+
+VS = 0
+afterBoolean = Boolean(VS)
+// console.log(typeof(afterBoolean))
+// console.log(afterBoolean)
+// console.log(VS)
+
+VS = null
+afterBoolean = Boolean(VS)
+// console.log(typeof(afterBoolean))
+// console.log(afterBoolean)
+// console.log(VS)
+
+let example 
+afterBoolean = Boolean(example)
+// console.log(typeof(afterBoolean))
+// console.log(afterBoolean)
+// console.log(example)
+
+let afterString = String(example)
+console.log(typeof(afterString))
+console.log(afterString)
+console.log(example)
---
+++ BasicJs/Datatype.js
@@ -0,0 +1,16 @@
+const name = new String("Ram")
+const name1 = "Ram1"
+name3 = "Ram3"; //must be avoided
+// console.log('This is name : '+name)
+// console.log('This is name : '+name1)
+// console.log('This is name : '+name3)
+// console.log('This is name : '+typeof(name))
+// console.log('This is name : '+typeof(name1))
+// console.log('This is name : '+typeof(name3))
+
+const age = 10
+const Isbig = true
+let decimal = 2.0867
+const exBigint1 = 347987329878735
+const exSymbol = new Symbol()
+console.log(exSymbol)
